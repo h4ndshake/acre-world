f27af2538ee9925a57e2d6b0b021e3b3abc8762f
Rafael Julio <Rafael_Julio11@hotmail.com>
2017-06-27 02:29:10 -0300

    Inúmeras melhorias na scene001

---
+++ Assets/Scripts/map001.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map001 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("Wait");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(120);
+
+		Application.LoadLevel("scene002");
+	}
+
+}
